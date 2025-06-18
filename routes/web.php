ca752c64d9e1d7849a32390e9c6fba34e1963ceb
Danski <unwill35@gmail.com>
2025-06-19 00:57:36 +0300

    a

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 Route::any('{any}', function () {
 
 })->where('any', '.*');
+
+Route::get('/health', function () {
+    return response('If you see this we are alive.', 200);
+});
