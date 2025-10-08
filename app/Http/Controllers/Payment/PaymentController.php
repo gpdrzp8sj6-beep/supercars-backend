e502ae316a800bd7206b87d40d7bb3066550134e
fullstack <fullstack@capecodes.com>
2025-10-08 18:31:04 +0500

    updated token

--- app/Http/Controllers/Payment/PaymentController.php
+++ app/Http/Controllers/Payment/PaymentController.php
@@ -160,7 +160,7 @@ class PaymentController extends Controller
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                           'Authorization: Bearer OGFjOWE0Y2M5NjYyYWIxZDAxOTY2ODdkNjFhMjI5MzN8UWltamM6IWZIRVpBejMlcnBiZzY='));
+                           'Authorization: Bearer MjRkYWM1YWItZjkyNy00YzBjLTgwZjctMDEzZDZiY2MxN2I4Omw4amxvaURKeEg='));
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);// this should be set to true in production
